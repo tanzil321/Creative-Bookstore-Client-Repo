0fab25125ff3ffc4441da58de1fc443098557839
tanzil321 <tanzilwhatever@gmail.com>
2022-11-30 23:53:37 +0600

    token updated

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -1,45 +1,53 @@
-import React, { useContext } from 'react';
-import { Link, Outlet } from 'react-router-dom';
-import { AuthContext } from '../contexts/AuthProvider';
-import useAdmin from '../hooks/useAdmin';
-import Navbar from '../Pages/Shared/Navbar/Navbar';
+import React, { useContext } from "react";
+import { Link, Outlet } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthProvider";
+import useAdmin from "../hooks/useAdmin";
+import useSeller from "../hooks/useSeller";
+import Navbar from "../Pages/Shared/Navbar/Navbar";
 
 const DashboardLayout = () => {
-    const { user,loading } = useContext(AuthContext);
-    // const [isAdmin] = useAdmin(user?.email) 
-    
-    
-    
-    return (
-        <div>
-            <Navbar></Navbar>
-            <div className="drawer drawer-mobile">
-                <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content">
-                    <Outlet></Outlet>
-                </div>
-                <div className="drawer-side">
-                    <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 text-base-content">
+  const { user, loading } = useContext(AuthContext);
+  const [isAdmin] = useAdmin(user?.email);
+    const [isSeller] = useSeller(user?.email)
+   return (
+    <div>
+      <Navbar></Navbar>
+      <div className="drawer drawer-mobile">
+        <input
+          id="dashboard-drawer"
+          type="checkbox"
+          className="drawer-toggle"
+        />
+        <div className="drawer-content">
+          <Outlet></Outlet>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 text-base-content">
+            {isAdmin && (
+              <>
+                <li>
+                  <Link to="/dashboard/allseller">All Sellers</Link>
+                </li>
+                <li>
+                  <Link to="/dashboard/allbuyer">All Buyers</Link>
+                </li>
+              </>
+            )}
 
-                    
-                                <li><Link to='/dashboard/allseller'>All Sellers</Link></li>
-                                <li><Link to='/dashboard/allbuyer'>All Buyers</Link></li>
-                         
-                        
+            <li>
+              <Link to="/dashboard">My Orders</Link>
+            </li>
 
-                        
-                         <li><Link to='/dashboard'>My Orders</Link></li>
-                        
-                        
-                                <li><Link to='/dashboard/addproduct'>Add a Products</Link></li>
-                                <li><Link to='/dashboard/myproduct'>My Products</Link></li>
-                            
-                        
+                      
+            <li>
+              <Link to="/dashboard/addproduct">Add a Products</Link>
+            </li>
+            <li>
+              <Link to="/dashboard/myproduct">My Products</Link>
+            </li>
 
-
-                                
-                                {/* <li><Link to="/dashboard">My Orders</Link></li>
+            {/* <li><Link to="/dashboard">My Orders</Link></li>
                         
                             
                                 <li><Link to="/dashboard/alluser">All users</Link></li>
@@ -47,15 +55,11 @@ const DashboardLayout = () => {
                                 <li><Link to="/dashboard/addproduct">Add A product</Link></li>
                                 <li><Link to="/dashboard/allseller">All Sellers</Link></li>
                                 <li><Link to="/dashboard/allbuyer">All Buyers</Link></li> */}
-                           
-                        
-
-                    </ul>
-
-                </div>
-            </div>
+          </ul>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
---
+++ src/hooks/useSeller.js
@@ -0,0 +1,21 @@
+import { useState } from "react"
+import { useEffect } from "react"
+
+
+const useSeller = email =>{
+  const [isBuyer,setIsBuyer] = useState(false)
+  const [isBuyerLoading,setIsBuyerLoading] = useState(true)
+    useEffect(()=>{
+        if(email){
+            fetch(`https://bike-hut-server.vercel.app/user/buyer/${email}`)
+        .then(res=>res.json())
+        .then(data=>{
+            setIsBuyer(data.isSeller)
+            setIsBuyerLoading(false)
+        })
+        }
+    },[email])
+    return [isBuyer,isBuyerLoading];
+}
+
+export default useSeller;
